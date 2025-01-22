ee6585c0bee27bf7e6ead886594223bb98a6bb9e
rena <68492530+minahow3@users.noreply.github.com>
2025-01-22 13:21:59 +0000

    npm install cors バックエンドindex.jsにcorsを設定

--- test_F-EonDocker/backend/index.js
+++ test_F-EonDocker/backend/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const cors = require('cors');
+app.use(cors());
+
 app.get('/api/data', (req, res) => {
   res.json({ message: 'Hello from Express API!' });
 });
