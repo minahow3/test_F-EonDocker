d2d98e02b0b310442547ddbefbd4ae70f2340547
rena <68492530+minahow3@users.noreply.github.com>
2025-01-22 14:19:52 +0000

    バックエンドから見たフロントエンドのURLに修正

--- test_F-EonDocker/backend/index.js
+++ test_F-EonDocker/backend/index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 5000;
 
 const cors = require('cors');
 app.use(cors({
-  origin: 'http://frontend:3000', // フロントエンドのURLを指定
+  origin: 'http://localhost:3000', // フロントエンドのURLを指定
 }));
 
 app.get('/api/data', (req, res) => {
