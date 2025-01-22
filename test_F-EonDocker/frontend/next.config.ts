4cbe18c242bcd8220a6d06dec86a6fd78b43ee91
rena <68492530+minahow3@users.noreply.github.com>
2025-01-22 14:19:33 +0000

    「Mixed Content」問題対策のためにプロキシ設定を追加

--- test_F-EonDocker/frontend/next.config.ts
+++ test_F-EonDocker/frontend/next.config.ts
@@ -1,8 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  
+  // プロキシ設定を追加
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*', // フロントエンドの /api/* に対するリクエスト
+        destination: 'http://backend:5000/api/:path*', // バックエンドの API エンドポイント
+      },
+    ];
+  },
 };
 
 export default nextConfig;
