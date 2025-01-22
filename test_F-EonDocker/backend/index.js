f7347377ab2d396474a3d8478482c02cc6fd8c37
rena <68492530+minahow3@users.noreply.github.com>
2025-01-22 14:59:01 +0000

    CORS詳細設定を追加

--- test_F-EonDocker/backend/index.js
+++ test_F-EonDocker/backend/index.js
@@ -3,9 +3,15 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 const cors = require('cors');
-app.use(cors({
-  origin: 'http://localhost:3000', // フロントエンドのURLを指定
-}));
+
+// CORS設定をより明示的に指定
+const corsOptions = {
+  origin: 'https://miniature-giggle-55rw9q46x9j279rp-3000.app.github.dev', // フロントエンドのURL
+  methods: ['GET', 'POST'], // 許可するHTTPメソッド
+  allowedHeaders: ['Content-Type'], // 許可するヘッダー
+};
+
+app.use(cors(corsOptions));
 
 app.get('/api/data', (req, res) => {
   res.json({ message: 'Hello from Express API!' });
